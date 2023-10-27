cab2db7e399e17c4e1d906d051b6d77a046bc85b
zhengzhang <zhengzhang@ruijie.com.cn>
2023-10-27 17:32:13 +0800

    electron-demo

--- vue-study/vue2-project/electron-demo/main.js
+++ vue-study/vue2-project/electron-demo/main.js
@@ -1,13 +1,26 @@
-const { app, BrowserWindow } = require("electron");
-let mainWindow;
- 
-app.on("ready", () => {
-  mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 680,
-    webPreferences: {
-      nodeIntegration: true
-    },
-  });
-  mainWindow.loadFile('index.html')
-});
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {   
+  // 创建浏览器窗口
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+	webPreferences: {
+		preload: path.join(__dirname, 'preload.js'),
+		nodeIntegration: false,
+		devTools: true,
+		webSecurity:false,
+		experimentalFeatures:true,
+		contextIsolation:false
+	}
+
+  })
+
+  // 加载index.html文件
+  //win.loadFile('index.html')
+  win.loadFile('index-desktop.html')
+}
+
+ // 应用程序准备就绪后打开一个窗口
+app.whenReady().then(createWindow)
---
+++ vue-study/vue2-project/electron-demo/preload.js
@@ -0,0 +1,19 @@
+window.navigator.mediaDevices.getDisplayMedia = () => {  
+    return new Promise(async (resolve, reject) => {
+      try {
+        const sources = await desktopCapturer.getSources({ types: ['screen'] })
+        const stream = await window.navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+              mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: sources[0].id
+              }
+            }
+          })
+          resolve(stream)
+        }catch(err){
+            reject(err)
+        }       
+    })
+  }
